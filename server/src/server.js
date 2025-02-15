f6ab5cea76b9463c2ed21a068b5932baf56b6738
Luc <elyktrar@gmail.com>
2025-02-15 14:42:45 -0700

    modified:   server/package-lock.json 	modified:   server/package.json 	new file:   server/src/auth/google-auth.js 	new file:   server/src/auth/local-auth.js 	new file:   server/src/helpers/encryption.js 	modified:   server/src/mongoDB/User.js 	modified:   server/src/server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
 import { connectMongo } from "./mongoDB/connectMongo.js";
 import express from "express";
-import http from "node:http"
+import http from "node:http";
+import bodyParser from "body-parser";
 dotenv.config();
 
-const app = express()
+const app = express();
+app.use(bodyParser.json());
 
-const server = http.createServer(app)
 
-connectMongo()
+const server = http.createServer(app);
+
+connectMongo();
 
 server.listen(process.env.PORT, () => {
-  console.log(`listening on PORT: ${process.env.PORT}`)
-})
+  console.log(`listening on PORT: ${process.env.PORT}`);
+});
