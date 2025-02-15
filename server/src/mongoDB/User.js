e91769b791580b746bbc4907657b5434e3d38382
Luc <elyktrar@gmail.com>
2025-02-15 15:04:57 -0700

    yea yea User schema

--- server/src/mongoDB/User.js
+++ server/src/mongoDB/User.js
@@ -1,1 +1,28 @@
 //create user Schema
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: false,
+  },
+  hash: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: false,
+  },
+  email: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: true,
+  },
+  provider: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
