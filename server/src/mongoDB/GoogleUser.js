2fe14e4df861f92de311a8fb369bf36745f7f598
Luc <elyktrar@gmail.com>
2025-02-15 16:22:53 -0700

    google auth yes

---
+++ server/src/mongoDB/GoogleUser.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const GoogleUserSchema = {
+  username: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: false,
+  },
+  email: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: true,
+  },
+  authID: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: true,
+  },
+  provider: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: false,
+  },
+  profilePicture: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: false,
+  },
+};
+
+const GoogleUser = mongoose.model("GoogleUser", GoogleUserSchema);
+
+export default GoogleUser;
