97bc41b5af9fb1e7d6c7c399c97234debf5cfc7e
Luc <elyktrar@gmail.com>
2025-02-15 14:56:07 -0700

    express session, gonna implement auth soon

--- server/src/mongoDB/connectMongo.js
+++ server/src/mongoDB/connectMongo.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose"
 export const connectMongo = async() => {
   try{
     await mongoose.connect(process.env.MONGO_URI)
-    console.log(`connected to database :${process.env.MONGO_URI}`)
+    console.log(`connected to database: ${process.env.MONGO_URI}`)
   } catch (e) {
     console.error(e)
   }
--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,22 @@ import { connectMongo } from "./mongoDB/connectMongo.js";
 import express from "express";
 import http from "node:http";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+  },
+}))
 
 const server = http.createServer(app);
 
