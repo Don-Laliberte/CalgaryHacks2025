86feeddb01487c44ab5cd57cfb1bb82f783a147d
Don Laliberte <donhenrylaliberte@gmail.com>
2025-02-15 20:54:14 -0700

    Background image added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -11,22 +11,22 @@ const HomePage = () => {
       <img 
         src="/images/background.png" 
         alt="background" 
-        className="absolute inset-0 w-full h-full object-cover"
+        className="background-image"
       />
       
-      {/* Semi-transparent overlay */}
+      {/* Semi-transparent overlay
       <div className="absolute inset-0 bg-black/20" />
       
       {/* Button container */}
-      <div className="absolute inset-0 flex items-center justify-center">
-        <button 
+      {/* <div className="absolute inset-0 flex items-center justify-center"> */}
+        {/* <button 
           className="bg-green-600 hover:bg-green-700 text-white text-xl font-bold py-4 px-8 rounded-lg transition-all duration-200 hover:scale-105 shadow-lg z-10"
           onClick={() => navigate('/quiz')}
         >
           Start Quiz
-        </button>
-      </div>
-    </div>
+        </button> */}
+      {/* </div> */}
+    </div> 
   );
 };
 
