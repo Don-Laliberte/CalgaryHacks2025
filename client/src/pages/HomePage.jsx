1cd9fa1099a256c83c88cb4f4574f8d09c81fef5
Don Laliberte <donhenrylaliberte@gmail.com>
2025-02-15 23:57:51 -0700

    Added Scrolling

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,26 +6,12 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="relative w-full h-full">
-      {/* Background Container */}
+    <div className="home-container">
       <img 
-        src="/images/background-upscaled.jpg" 
+        src="/background-scrollable.png" 
         alt="background" 
         className="background-image"
       />
-      
-      {/* Semi-transparent overlay
-      <div className="absolute inset-0 bg-black/20" />
-      
-      {/* Button container */}
-      {/* <div className="absolute inset-0 flex items-center justify-center"> */}
-        {/* <button 
-          className="bg-green-600 hover:bg-green-700 text-white text-xl font-bold py-4 px-8 rounded-lg transition-all duration-200 hover:scale-105 shadow-lg z-10"
-          onClick={() => navigate('/quiz')}
-        >
-          Start Quiz
-        </button> */}
-      {/* </div> */}
     </div> 
   );
 };
